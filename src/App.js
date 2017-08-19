64be48af16bd20d811f70bf52053e100db60577d
Scott Young <young.scottw@gmail.com>
2017-08-19 14:13:00 -0500

    Add airplane logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import airplaneLogo from './img/airplane-logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={airplaneLogo} className="App-logo" alt="logo" />
+          <h2>Welcome to Av-Data</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Powered by FlightAware FlightXML 3
         </p>
       </div>
     );
