8b61bc54ca0e43cb82d4f92d76ccd731e64fec40
Scott Young <young.scottw@gmail.com>
2017-08-20 15:27:42 -0500

    Styling and cleanup

--- src/components/AirportInfo.js
+++ src/components/AirportInfo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class AirportInfo extends Component {
     render() {
         const airport = this.props.airport;
-        console.log(airport);
+
         return (
             <div>
                 <h1>{airport.airport_code}</h1>
